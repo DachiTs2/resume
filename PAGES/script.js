f310600e295152b9a9257c50d98c248ee4217c9e
DachiTs2 <124523353+DachiTs2@users.noreply.github.com>
2025-08-29 18:16:42 +0400

    js

---
+++ PAGES/script.js
@@ -0,0 +1,10 @@
+document.getElementById("home").onclick = function() {
+    window.location.href = "main.html"; 
+};
+
+document.getElementById("about").onclick = function() {
+    window.location.href = "about.html"; 
+};
+document.getElementById("resume").onclick = function() {
+    window.location.href = "resume.html"; 
+};
